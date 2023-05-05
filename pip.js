6a79fe8724065bd5894327d97826d5e55bbca8ed
orishaba-mercy <orishabamercybanga@gmail.com>
2023-05-05 20:42:15 +0300

    changes

--- pip.js
+++ pip.js
@@ -23,22 +23,24 @@ console.log(get_sum(nums))
 
 
 // Write a function that takes two numbers as input and returns true if their sum is greater than 100, and false otherwise.
-function greaterNumbers(num1,num2){
-    let sum =num1+num2
+function greatNumbers(number1,number2){
+    let sum =number1+number2
     if(sum>100){
         return "true"
     }else {
         return "false"
     }
 }
-console.log(greaterNumbers(60,30));
+console.log(greaterNum(30,40));
 // Write a function that takes in a array of numbers as a parameter and returns the second largest number in the array
 function getPalindrome(name){
     const len = name.length;    
     for (let i = 0; i < len / 2; i++) {
         if (name[i] == name[len - 1 - i]) {
             return "true";
-        }else{
+
+        }
+        else{
            return "false";
         }
 }
