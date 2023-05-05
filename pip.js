a2b71865281213ea2c9ae5132dd4f43440045a57
orishaba-mercy <orishabamercybanga@gmail.com>
2023-05-05 20:37:39 +0300

    first commit

---
+++ pip.js
@@ -0,0 +1,52 @@
+// Write a function that takes a string as input and returns the number of vowels in the string.
+function getVowels(word){
+    const count=word.match(/[a,e,i,o,u]/gi).length
+    return count
+    
+}
+let word="orishaba"
+console.log(getVowels(word))
+
+
+// Write a function that takes an array of numbers as input and returns the sum of all the numbers.
+function get_sum(){
+    let nums=[1,2,3,4,5,6]
+   sum =0;
+   for (let i =0;i<=nums.length;i++){
+    sum+=nums[i];
+    
+   }
+   return sum;
+}
+console.log(get_sum(nums))
+   
+
+
+// Write a function that takes two numbers as input and returns true if their sum is greater than 100, and false otherwise.
+function greaterNumbers(num1,num2){
+    let sum =num1+num2
+    if(sum>100){
+        return "true"
+    }else {
+        return "false"
+    }
+}
+console.log(greaterNumbers(60,30));
+// Write a function that takes in a array of numbers as a parameter and returns the second largest number in the array
+function getPalindrome(name){
+    const len = name.length;    
+    for (let i = 0; i < len / 2; i++) {
+        if (name[i] == name[len - 1 - i]) {
+            return "true";
+        }else{
+           return "false";
+        }
+}
+}
+console.log(getPalindrome("noon"));
+
+
+
+
+
+
